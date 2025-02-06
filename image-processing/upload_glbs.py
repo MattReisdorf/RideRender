20bea4de7acdb6aabe38130387ccbd69a8f53468
MattReisdorf <mreisdorf9717@gmail.com>
2025-02-06 13:18:34 -0500

    skip over models already in FB, saves data usage

--- image-processing/upload_glbs.py
+++ image-processing/upload_glbs.py
@@ -14,7 +14,12 @@ firebase_admin.initialize_app(cred, {
 
 bucket = storage.bucket()
 
+# Get the models already in FB
+existing_blobs = []
+for blob in bucket.list_blobs():
+  existing_blobs.append(blob.name);
 
+print(existing_blobs);
 base_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'boards')
 for brand in os.listdir(base_path):
   brand_path = os.path.join(base_path, brand)
@@ -23,7 +28,13 @@ for brand in os.listdir(base_path):
     for model in os.listdir(board_path):
       if '.glb' not in model:
         continue
-      file_path = os.path.join(base_path, brand, board, model).replace("\\", '/')
-      blob = bucket.blob(model)
-      blob.upload_from_filename(file_path)
-      print(f"File {file_path} uploaded to gs://{bucket.name}/{model}")
+      
+      # Skip over models already in FB
+      if model in existing_blobs:
+        print(model, "already in existing_blobs")
+
+      else:
+        file_path = os.path.join(base_path, brand, board, model).replace("\\", '/')
+        blob = bucket.blob(model)
+        blob.upload_from_filename(file_path)
+        print(f"File {file_path} uploaded to gs://{bucket.name}/{model}")
